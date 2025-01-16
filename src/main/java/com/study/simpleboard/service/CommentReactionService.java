8318cf296e93d7fd2c88e325cc12367898b1f1a4
0515-Yoonseo-Kim <qnq0615@gmail.com>
2025-01-16 21:01:11 +0900

    feat: #2 댓글 반응 서비스 추가

---
+++ src/main/java/com/study/simpleboard/service/CommentReactionService.java
@@ -0,0 +1,31 @@
+package com.study.simpleboard.service;
+
+import com.study.simpleboard.domain.TargetType;
+import com.study.simpleboard.dto.CommentReactionDTO;
+import com.study.simpleboard.mapper.CommentReactionMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentReactionService {
+
+    private final CommentReactionMapper commentReactionMapper;
+
+    public CommentReactionService(CommentReactionMapper commentReactionMapper) {
+        this.commentReactionMapper = commentReactionMapper;
+    }
+
+    public void addCommentReaction(Long commentId, CommentReactionDTO commentReactionDTO){
+        CommentReactionDTO reaction = new CommentReactionDTO(
+                commentReactionDTO.reactionId(),
+                commentReactionDTO.userId(),
+                commentId,
+                TargetType.COMMENT,
+                commentReactionDTO.reactionType(),
+                commentReactionDTO.isActive()
+        );
+        commentReactionMapper.insertCommentReaction(reaction);
+    }
+
+}
