d1e3e5c0b934a80f270e906afb3f228dd47f82cf
minhyung <minhyung66547@gmail.com>
2025-01-16 20:25:26 +0900

    feat: #4 게시판 좋아요/싫어요 repository 추가

---
+++ src/main/java/com/study/simpleboard/domain/Reaction.java
@@ -0,0 +1,31 @@
+package com.study.simpleboard.domain;
+
+import com.study.simpleboard.enums.ReactionType;
+import com.study.simpleboard.enums.TargetType;
+import lombok.*;
+
+@Getter
+@RequiredArgsConstructor
+public class Reaction {
+    private final Long id;
+    private final Long userId;
+    private final Long targetId;
+    private final TargetType targetType;
+    private final ReactionType reactionType;
+    private final Boolean active;
+
+    @Builder
+    public Reaction(Long userId, Long targetId, TargetType targetType, ReactionType reactionType, Boolean active) {
+        this(null, userId, targetId, targetType, reactionType, active);
+    }
+
+    public Reaction changeActive(ReactionType reactionType, Boolean active) {
+        return Reaction.builder()
+                .userId(userId)
+                .targetId(targetId)
+                .targetType(targetType)
+                .reactionType(reactionType)
+                .active(active)
+                .build();
+    }
+}
---
+++ src/main/java/com/study/simpleboard/enums/TargetType.java
@@ -0,0 +1,5 @@
+package com.study.simpleboard.enums;
+
+public enum TargetType {
+    POST, COMMENT
+}
