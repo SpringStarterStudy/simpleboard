81f3b29bfee80abd074ced9b130a88f832bffbc5
yurim <y1551463@gmail.com>
2025-01-17 22:04:25 +0900

    chore : #5 주석 추가

--- src/main/java/com/study/simpleboard/common/exception/ErrorCode.java
+++ src/main/java/com/study/simpleboard/common/exception/ErrorCode.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
+    // 서버
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."),
 
+    // 댓글
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 존재하지 않습니다."),
     NO_COMMENT_AUTHORITY(HttpStatus.FORBIDDEN, "해당 댓글의 작성자가 아닙니다.");
 
