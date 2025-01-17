374fe70741946da13c0131c8891337dc2f2a1311
yurim <y1551463@gmail.com>
2025-01-17 23:38:42 +0900

    refactor : #5 댓글 유효성 검사 추가

--- src/main/java/com/study/simpleboard/dto/CommentCreateRequestDTO.java
+++ src/main/java/com/study/simpleboard/dto/CommentCreateRequestDTO.java
@@ -1,8 +1,10 @@
 package com.study.simpleboard.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CommentCreateRequestDTO {
+    @Size(max = 200, message = "댓글은 200자 이내로만 작성 가능합니다.")
     private String commentContent;
 }
