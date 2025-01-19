cb6b04fda5f5c7f55b02a07483289619b2654497
minhyung <minhyung66547@gmail.com>
2025-01-19 17:28:21 +0900

    test: #4 controller 테스트 코드 추가
    - json 변환을 위해 gson 의존성 추가
    - 테스트 중 객체 비교를 위해 @EqualsAndHashCode 추가

--- src/main/java/com/study/simpleboard/dto/post/PostReactionReq.java
+++ src/main/java/com/study/simpleboard/dto/post/PostReactionReq.java
@@ -1,10 +1,12 @@
 package com.study.simpleboard.dto.post;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@EqualsAndHashCode
 public class PostReactionReq {
     private final Long userId;
     private final Boolean like;
