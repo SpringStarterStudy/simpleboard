afd35054c08d86d9e6633153a02e2ae1f06e3229
dayeon <99ekdus@naver.com>
2025-01-20 17:55:33 +0900

    test : SignUpRequestTest 코드 추가

--- src/test/java/com/study/simpleboard/dto/request/SignUpRequestTest.java
+++ src/test/java/com/study/simpleboard/dto/request/SignUpRequestTest.java
@@ -24,7 +24,7 @@ class SignUpRequestTest {
     }
 
     @Test
-    @DisplayName("유효한 이름입니다.")
+    @DisplayName("유효한 이름이다.")
     void validName() {
         // given
         SignUpRequest request = createValidRequest();
@@ -34,7 +34,102 @@ class SignUpRequestTest {
 
         // then
         assertThat(violations).isEmpty();
+    }
 
+    @Test
+    @DisplayName("이름이 null 값일 때 회원가입에 실패한다.")
+    void validName2() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setName(null);
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("이름은 필수 입력값입니다.");
+    }
+
+    @Test
+    @DisplayName("이름이 한 글자일 때 회원가입에 실패한다.")
+    void validName3() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setName("김");
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("이름은 2자 이상 9자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("이름에 허용되지 않은 특수문자가 들어가면 회원가입에 실패한다.")
+    void validname4() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setName("테스트!");
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("이름은 한글, 영문, 숫자, 언더바(_)만 사용 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 이메일 형식을 입력하면 회원가입 실패를 한다.")
+    void validEmail() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setEmail("test-email");
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("올바른 이메일 형식이 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호에는 특수문자가 포함되지 않으면 회원가입 실패를 한다.")
+    void validPassword2() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setPassword("Abcd123456");
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("비밀번호는 특수문자를 포함해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("올바른 휴대폰 번호 형식이 아니면 회원가입 실패를 한다.")
+    void validCellPhone() {
+        // given
+        SignUpRequest request = createValidRequest();
+        request.setCellPhone("010-1234-5678"); // 하이픈이 들어가면 안됨
+
+        // when
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations)
+                .extracting("message")
+                .contains("올바른 휴대폰 번호 형식이 아닙니다.");
     }
 
 
