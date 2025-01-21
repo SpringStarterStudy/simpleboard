06938b586d03445447b8d9b35697bdac0a49dfee
dayeon <99ekdus@naver.com>
2025-01-21 17:40:32 +0900

    refactor : ApiResponse 에러 처리 메서드 추가

---
+++ src/main/java/com/study/simpleboard/common/response/ApiResponse.java
@@ -0,0 +1,47 @@
+package com.study.simpleboard.common.response;
+
+import com.study.simpleboard.common.exception.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiResponse<T> {
+    private final int status;
+    private final String message;
+    private final T data;
+
+    private ApiResponse(HttpStatus status, String message, T data) {
+        this.status = status.value();
+        this.message = message;
+        this.data = data;
+    }
+
+    private ApiResponse(HttpStatus status, String message) {
+        this(status, message, null);
+    }
+
+    // 아래 메서드 수정
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>(HttpStatus.OK, "success");
+    }
+
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(HttpStatus.OK, message);
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(HttpStatus.OK, "success", data);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(HttpStatus.OK, message, data);
+    }
+
+    public static ApiResponse<Void> error(HttpStatus status, String message) {
+        return new ApiResponse<>(status, message);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<>(errorCode.getStatus(), errorCode.getMessage());
+    }
+}
