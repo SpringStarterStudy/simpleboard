b0fbd82bc6fd4d72027d233e47d6d09059bea0f3
minhyung <minhyung66547@gmail.com>
2025-01-25 00:28:24 +0900

    feat: #4 파라미터 검증을 위한 ExceptionHandler 추가
    - ConstraintViolationException을 처리하는 핸들러 추가
    - 필드명과 에러 메시지를 추출하여 에러 응답 제공

--- src/main/java/com/study/simpleboard/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study/simpleboard/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.study.simpleboard.common.exception;
 
 import com.study.simpleboard.common.response.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -33,5 +36,19 @@ public class GlobalExceptionHandler {
                 bindingResult.getAllErrors().get(0).getDefaultMessage()));
     }
 
+    // ValidationException 처리
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleConstraintViolationException(ConstraintViolationException ex) {
+        String errorMessage = ex.getConstraintViolations().stream()
+                .map(violation -> {
+                    String fullPath = violation.getPropertyPath().toString();
+                    String field = fullPath.substring(fullPath.lastIndexOf(".") + 1); // 필드명만 추출
+                    log.error("Validation 예외 발생 {}: {}", fullPath, violation.getMessage());
+                    return field + ": " + violation.getMessage();
+                })
+                .collect(Collectors.joining(", "));
 
+        return ResponseEntity.badRequest().body(
+                ApiResponse.error(HttpStatus.BAD_REQUEST, errorMessage));
+    }
 }
