d6428e116d1bd838c75154cfd4de68f8941ca919
minhyung <minhyung66547@gmail.com>
2025-01-24 21:48:03 +0900

    refactor: #15 PostCreateReq 수정
    - 테스트 코드에서 객체 비교가 필요해서 @EqualsAndHashCode 추가

--- src/main/java/com/study/simpleboard/dto/PostCreateReq.java
+++ src/main/java/com/study/simpleboard/dto/PostCreateReq.java
@@ -3,11 +3,13 @@ package com.study.simpleboard.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@EqualsAndHashCode
 public class PostCreateReq {
     // 나중에 인증 구현 후 수정 예정이라 임시로 만듦
     @NotNull
