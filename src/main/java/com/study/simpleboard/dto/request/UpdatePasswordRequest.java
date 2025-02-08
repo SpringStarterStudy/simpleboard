8e9e6c4baea366b97a5523924be0333e8e05de44
dayeon <99ekdus@naver.com>
2025-02-08 18:25:25 +0900

    chore : 비밀번호 최대 길이 제한 추가

--- src/main/java/com/study/simpleboard/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/study/simpleboard/dto/request/UpdatePasswordRequest.java
@@ -16,7 +16,7 @@ public class UpdatePasswordRequest {
     private String currentPassword;
 
     @NotNull(message = "새 비밀번호는 필수 입력값입니다.")
-    @Size(min = 10, message = "비밀번호는 10자 이상이어야 합니다.")
+    @Size(min = 10, max = 72, message = "비밀번호는 10자 이상이어야 합니다.")
     @Pattern.List({
             @Pattern(regexp = ".*[!@#$%^&*(),.?\":{}|<>].*", message = "비밀번호는 특수문자를 포함해야 합니다."),
             @Pattern(regexp = ".*[A-Z].*", message = "비밀번호는 대문자를 포함해야 됩니다."),
