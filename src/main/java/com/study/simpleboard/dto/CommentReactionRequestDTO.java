4a43331f1ba98015afc64bb67cf9db8c3945f6e3
0515-Yoonseo-Kim <qnq0615@gmail.com>
2025-02-09 16:20:56 +0900

    feat: 검증 로직 추가

--- src/main/java/com/study/simpleboard/dto/CommentReactionRequestDTO.java
+++ src/main/java/com/study/simpleboard/dto/CommentReactionRequestDTO.java
@@ -1,6 +1,7 @@
 package com.study.simpleboard.dto;
 
 import com.study.simpleboard.domain.enums.ReactionType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
